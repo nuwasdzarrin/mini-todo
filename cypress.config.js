abeb84f5d938b0901977b34e84821ada3f23f76b
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2023-01-05 12:53:40 +0700

    add and run cypress testing

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 's7stqh',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
